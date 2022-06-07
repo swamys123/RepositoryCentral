1809aaf97a34c30d2aa5f4bf0ee0c860335e8d9c
N S Swamys <nsswamys@gmail.com>
2022-06-07 19:31:30 +0530

    Repo project added

---
+++ src/main/java/io/github/swamys123/repository/App.java
@@ -0,0 +1,13 @@
+package io.github.swamys123.repository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
